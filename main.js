d8f3432a14af1135c3b5cd452d5658593d35db2c
vijaysingh991 <vijay.nimar@neudayai.com>
2026-09-06 15:15:59 +0530

    health router

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ app.use(cors())
 app.use(express.json())
 app.use("/user",userRoute)
 const port=3001
-
+app.get("/health",(req,res)=>{
+    return res.json({status:true,time:new Date(),message:"server running perfectly"})
+})
 app.listen(port,async()=>{
     await connection()
     console.log(`server started at port - > ${port}`)
